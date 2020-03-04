c7e799679f273908554a51854991fa3a2ea0d249
Hao Chen <chenhao@microduino.cc>
2020-03-04 13:21:34 +0800

    us ts-node

--- node/mnist/dataGz.ts
+++ node/mnist/dataGz.ts
@@ -16,11 +16,11 @@
  */
 
 import * as tf from '@tensorflow/tfjs-node'
-import { fetchLocal, logger } from './utils'
+import { fetchLocal, logger } from '../utils'
 
 // MNIST data constants:
 // const BASE_URL = 'https://storage.googleapis.com/cvdf-datasets/mnist/';
-const BASE_URL = './data'
+const BASE_URL = '../../public/preload/data/mnist'
 const TRAIN_IMAGES_FILE = `${BASE_URL}/train-images-idx3-ubyte.gz`
 const TRAIN_LABELS_FILE = `${BASE_URL}/train-labels-idx1-ubyte.gz`
 const TEST_IMAGES_FILE = `${BASE_URL}/t10k-images-idx3-ubyte.gz`
